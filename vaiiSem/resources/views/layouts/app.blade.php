5a55e8c658896aac0140feef850adc7f750a5cad
Si13ncee <kuba1597@gmail.com>
2023-12-03 23:09:54 +0100

    WIP admin/user dashboards

--- vaiiSem/resources/views/layouts/app.blade.php
+++ vaiiSem/resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,11 +17,13 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    
     
 
 
 </head>
+
+
 <body>
     <div id="app">
         <!-- Navbar -->
@@ -27,7 +31,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">{{ config('Tech H4ven', 'Tech H4ven') }}</a>
             
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
